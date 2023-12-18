b6a6f3375fcfdaf58c24a3c6b38428978a58d462
zs0732 <zs0732@bs.amalnet.k12.il>
2023-12-18 15:13:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/exer2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.exer2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int x;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+
+    }
+
+    public void lol(View view) {
+        x++;
+        btn.setText("This is a click number:"+x);
+
+    }
+}
